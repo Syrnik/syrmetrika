b8c979135afdf165c2d20525267a852ba7e76f1f
Serge Rodovnichenko <sergerod@gmail.com>
2014-10-04 01:49:02 +0400

    Initial commit

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Send orders parameters to Yandex.Metrika',
+  'icon' => 'img/syrmetrika.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopSyrmetrikaPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopSyrmetrikaPlugin extends shopPlugin
+{
+
+}
