79bf7d72431a3372866ab5d32d63f7d40cd56cce
Serge Rodovnichenko <sergerod@gmail.com>
2015-03-09 21:05:18 +0300

    Локализация

--- js/syrmetrika.js
+++ js/syrmetrika.js
@@ -11,6 +11,7 @@ $(function () {
         settlement: $('[name="settlement"]', '#syrmetrikasettings'),
         load: function () {
             var settlement = $.Syrmetrika.settlement.val();
+            $('#syrmetrika-loading-settlement-data-icon').show();
             $('input,select,button', $.Syrmetrika.form).prop('disabled', true);
             $.get(
                 $.Syrmetrika.form.attr('action'),
@@ -22,25 +23,47 @@ $(function () {
                         $.each(r.data, function(key, value){
                             $("input[name='setting["+key+"]']", $.Syrmetrika.form).val(value);
                         })
+                    } else if(r.status == 'fail') {
+                        $('#syrmetrika-load-settlement-data-messages').text(r.errors[0]);
+                        $('#syrmetrika-load-settlement-data-messages').show('fast', function(){
+                            $(this).fadeOut(2000);
+                        });
                     }
                 },
                 'json')
                 .always(function () {
                     $('input,select,button', $.Syrmetrika.form).prop('disabled', false);
+                    $('#syrmetrika-loading-settlement-data-icon').hide();
                 });
         },
 
         save: function () {
             var form_data = $.Syrmetrika.form.serialize();
             $('input,select,button', $.Syrmetrika.form).prop('disabled', true);
+            $('#syrmetrika-save-settings-process-icon').show();
             $.post(
                 $.Syrmetrika.form.attr('action'),
                 form_data,
                 function (r) {
+                    if(r.status == 'ok') {
+                        $('#syrmetrika-save-settings-result')
+                            .removeClass()
+                            .addClass('success')
+                            .html('<i class="icon16 yes"></i> ' + r.data[0]);
+                    } else if(r.status == 'fail') {
+                        $('#syrmetrika-save-settings-result')
+                            .removeClass()
+                            .addClass('error-message')
+                            .html('<i class="icon16 no"></i> ' + r.errors[0])
+                    }
                 },
                 'json')
                 .always(function () {
                     $('input,select,button', $.Syrmetrika.form).prop('disabled', false);
+                    $('#syrmetrika-save-settings-process-icon').hide();
+                    $('#syrmetrika-save-settings-result').show('fast', function(){
+                        $(this).fadeOut(2000);
+                    });
                 });
         },
 
